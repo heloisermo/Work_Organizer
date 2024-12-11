d9f99d419bb455b31da6207e168c3d397219f0ab
heloisermo <heloise.romeo@edu.devinci.fr>
2024-12-11 15:34:17 +0100

    Déconnexion ok

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path : 'sign-in', component: SignInComponent},
     {path : 'sign-up', component: SignUpComponent},
-    { path: '', redirectTo: '/sign-up', pathMatch: 'full' }
+    {path : '', component: DashboardComponent},
 ];

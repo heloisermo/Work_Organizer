56d79f4c42d9e99c6e7ecf104fdbdad3a6f894a8
heloisermo <heloise.romeo@edu.devinci.fr>
2024-12-11 11:31:17 +0100

    Ajout du calendrier, et Ajout des taches dans le calendrier

--- Front/src/app/dashboard/dashboard.component.ts
+++ Front/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular'; // Module FullCalendar
+import timeGridPlugin from '@fullcalendar/timegrid'; // Plugin vue calendrier en grille horaire
+import interactionPlugin from '@fullcalendar/interaction';
+
+interface CalendarEvent {
+  title: string;
+  start: string;
+  allDay?: boolean;
+}
 
 interface Task{
   name: string;
@@ -12,28 +21,47 @@ interface Task{
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, FullCalendarModule], // Ajout du module FullCalendar
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   newTask: Task = { name: '', completed: false };
   tasks: Task[] = [];
+  calendarOptions: any = {
+    plugins: [timeGridPlugin, interactionPlugin],
+    initialView: 'timeGridDay',
+    editable: true,
+    droppable: true, 
+    events: [],};
 
-
-  addTask() {
-    if (this.newTask.name.trim()) {
-      this.tasks.push({ ...this.newTask }); 
-      this.newTask = { name: '', completed: false }; 
+    addTask() {
+      if (this.newTask.name.trim()) {
+        this.tasks.push({ ...this.newTask }); 
+        if (this.newTask.date) {
+          this.calendarOptions.events = [
+            ...this.calendarOptions.events,
+            {
+              title: this.newTask.name,
+              start: `${this.newTask.date}T${this.newTask.time ? this.newTask.time : '00:00:00'}`,
+              allDay: !this.newTask.time
+            }
+          ];
+        }
+        this.newTask = { name: '', completed: false }; 
+      }
     }
-  }
 
   toggleTask(task: Task){
     task.completed = !task.completed;
   }
 
-  deleteTask(task: Task){
+  deleteTask(task: Task) {
     this.tasks = this.tasks.filter(t => t !== task);
-    
+
+    this.calendarOptions.events = this.calendarOptions.events.filter((event: CalendarEvent) => 
+      event.title !== task.name || event.start !== `${task.date}T${task.time ? task.time : '00:00:00'}`
+    );
   }
+  
 }

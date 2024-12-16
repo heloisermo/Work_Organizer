51afac252284d8df31b6801d058317d122735e29
heloisermo <heloise.romeo@edu.devinci.fr>
2024-12-16 11:13:59 +0100

    Ajout d'utilisateurs depuis le front

--- Front/src/app/api.service.ts
+++ Front/src/app/api.service.ts
@@ -5,11 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root', 
 })
 export class ApiService {
-    private apiUrl = 'http://localhost:3000/users/list';
+    private apiUrl_select = 'http://localhost:3000/users/list';
+    private apiUrl_insert = 'http://localhost:3000/users/create';
     constructor(private http: HttpClient) {}
 
     getUsers()
     {
-        return this.http.get(this.apiUrl);
+        return this.http.get(this.apiUrl_select);
     }
+
+    createUser(user: any)
+    {
+        return this.http.post(this.apiUrl_insert, user);
+    }
+    
+
 }
--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { ApiService } from './api.service';
 import {HttpClient} from '@angular/common/http';
 import { provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root', 
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html', 
   styleUrls: ['./app.component.css'],
   providers: [ApiService, HttpClient]
@@ -16,14 +18,38 @@ export class AppComponent {
   constructor(private apiService: ApiService) {}
   title = 'Front';
   users: any[] = [];
+  email = '';
+  password = '';
   
   ngOnInit() {
-      this.apiService.getUsers().subscribe(
-      (response: any) => { this.users = response; },
-      (error) => { console.log(error); }
-      
+    this.addUser();
+  }
+
+  loadUsers() {
+    this.apiService.getUsers().subscribe(
+      (response: any) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
     );
-    console.log(this.users);
+  }
+  addUser() {
+    if (this.email && this.password) {
+      const newUser = { email: this.email, password: this.password };
+      this.apiService.createUser(newUser).subscribe(
+        (response) => {
+          console.log('Utilisateur ajouté avec succès!', response);
+          this.loadUsers();  // Recharge les utilisateurs après ajout
+        },
+        (error) => {
+          console.log('Erreur lors de l\'ajout de l\'utilisateur', error);
+        }
+      );
+    } else {
+      console.log('Email et mot de passe requis.');
+    }
   }
 
 }
